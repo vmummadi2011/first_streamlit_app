c02b66265ed856d59b03f1331a2fec735ad963f6
vmummadi2011 <venkatramana.mummadi@gmail.com>
2023-02-04 13:40:49 -0500

    Update streamlit_app.py
    added pandas library and pulled the text file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 
-
-streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -14,3 +9,8 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 
 streamlit.header(' 🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
