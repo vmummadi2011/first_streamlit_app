d5cff05797a17783cf34b5b1683fbd7fe321fda2
vmummadi2011 <venkatramana.mummadi@gmail.com>
2023-02-07 23:15:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 
 
 
-# streamlit.title ("My Mom's New Healthy Diner")
+streamlit.title ("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
