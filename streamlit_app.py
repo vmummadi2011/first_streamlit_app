c10a10e457091a31ad31f18b33536a553ccc2703
vmummadi2011 <venkatramana.mummadi@gmail.com>
2023-02-07 23:28:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,25 +48,24 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
 
- except URLError as e:
+except URLError as e:
     streamlit.error()
-        
+
+
 #New Section to display fruityvice api response
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 # deleted ... streamlit.text(fruityvice_response.json())  # just writes the data to the screen
 
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
